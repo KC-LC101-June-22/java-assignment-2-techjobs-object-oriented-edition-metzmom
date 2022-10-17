42fbe6b7bbd4fc2278119028f2d9314329e3975b
Margie Metz <metzmom8@gmail.com>
2022-10-17 11:02:49 -0500

    step 2 completed:location/coreCompetency/positiontype

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -18,6 +18,8 @@ public class CoreCompetency {
         this.value = value;
     }
 
+
+
     // Custom toString, equals, and hashCode methods:
 
     @Override
@@ -40,5 +42,15 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+    public String getValue() {
+        return value;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
